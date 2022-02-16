601bb4da867d034cf9d3af4cf9637520f7a355a0
Michael Viernes <92641515+MichaelViernes271@users.noreply.github.com>
2022-02-16 21:11:26 +0800

    Test & run scan_qrcode().
    commit e997ca7f5bb2006dd512ed9ab754b53944273b69
    Author: MichaelViernes271 <michaeleviernes@iskolarngbayan.pup.edu.ph>
    Date:   Wed Feb 16 20:43:53 2022 +0800
    
         On branch main
         Changes to be committed:
                modified:   MICHAEL_VIERNES.pdf
                modified:   qrcode-scanner.py
    
         Almost completed the tasks; from scanning to (almost) saving the file to a pdf.
         Formatting of the pdf is done yet the console needs a little modification.
    
    ...skipping [2nd commit (this is the 3rd commit)]...
    
         The program cannot run the usual openccv in W7 OS. Debugging through google-ing.
         The 'save_pdf()' is working fine.
         The sample qrcode for covid sheet is generated.

--- qrcode-scanner.py
+++ qrcode-scanner.py
@@ -1,3 +1,4 @@
+# PROGRAM: COVID TRACKING APPLICATION
 # Instruction: 
 # (1) Reads the qrcode of the client.
 # (2) Prints the information about the client.
@@ -6,7 +7,7 @@
 from fpdf import FPDF
 import time
 import cv2
-# from pyzbar.pyzbar import decode
+from pyzbar.pyzbar import decode
 
 # Saves data to pdf.
 # Parameters:
@@ -20,18 +21,56 @@ def save_pdf(name, data):
     
     # Set title.
     pdf.set_font('times', 'B', 16)
-    pdf.cell(12, 1, 'CONTACT TRACING SHEET', ln=1, border=1)
+    pdf.cell(20, 1, 'CONTACT TRACING SHEET', ln=1, border=1)
     
     # Set user data.
     pdf.set_font('times', '', 12)
     for u_data in data:
         pdf.cell(0, 1, u_data, ln=1)
-    name = upper(name) + ".pdf"
+    name = name.upper() + ".pdf"
     pdf.output(name)
+    
+    print('PDF SAVED!')
 # End of func.
     
 # Captures the qrcode and saves data to pdf.    
-# def scan_qrcode()
+def scan_qrcode():
+    qrcode = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    qrcode.set(3, 640)
+    qrcode.set(4, 480)
+
+    cam = True
+    while cam == True:
+        success, frame = qrcode.read() # not needed success variable 
+        
+        for user_data in decode(frame):
+            print("DECODED SUCCESSFULLY")
+            userinfo = user_data.data.decode('utf-8')
+            print('Type of Code: ', user_data.type,'\n')
+            print(userinfo)
+            userinfo = userinfo.split('\n')
+            pdf = input('Name saved data: ')
+            save_pdf(str(pdf), userinfo)
+            time.sleep(3)
+            cam = False
+            
+        cv2.imshow("QRCODE SCANNER", frame)    
+        cv2.waitKey(1)
 # End of func.    
  
-# scan_qrcode()
+def main():
+    scan_qrcode()
+# End of function.
+
+while True: # My template for usual main().
+    main()
+    quit = input("Quit (y/n): ")
+    if quit is type(str):
+        quit = quit.lower()
+        print(quit)
+    if (quit == 'y' or quit == 0):
+        print("Closing...\n")    
+        break
+# End of Func.
+
+exit() # Exits program.
